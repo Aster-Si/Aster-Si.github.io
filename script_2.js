181597a2a87acba4ec79cf6fbefdfb5ef00ff0c2
Aster-Si <159062845+Aster-Si@users.noreply.github.com>
2024-02-06 19:35:44 +0200

    Add files via upload

---
+++ script_2.js
@@ -0,0 +1,36 @@
+
+
+// let i = -1;
+// count = 0;
+// while (true) {
+//     i=i+2;
+//     count++
+//     if (count >= 55) {
+//         break
+//     }
+// console.log(i)
+// }
+// console.log(`Count: ${count}`)
+
+
+// let i = 90;
+// while (true) {
+//     i=i-5
+//     console.log(i)
+//         if (i<=0){
+//             break
+//     }
+// }
+
+let i = 2;
+count = 0;
+while (i<=10000000) {
+    
+    document.write(`<h2>Loop ${i}</h2>`);
+    i=i+i;
+    count++;
+    if (count >= 20){
+        break
+    }
+}
+console.log(`Loop ${count}`)
