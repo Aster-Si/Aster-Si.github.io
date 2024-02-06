c2d46d3b132b7445333d39fe35756b298f48fde2
Aster-Si <159062845+Aster-Si@users.noreply.github.com>
2024-02-06 19:40:34 +0200

    Update script 2.js

--- HW-2/script 2.js
+++ HW-2/script 2.js
@@ -1,8 +1,41 @@
-let ask = prompt('Ведіть факторіал');
-i = '';
-while(i>9999) {
-	console.log('ask'+i)
-	if (ask = 9999) {
-		break
-	}
+// let i = 997; 
+// while (i >= 9999) { 
+//     i=i+3
+//     console.log(i);
+// }
+
+
+// let i = -1;
+// count = 0;
+// while (true) {
+//     i=i+2;
+//     count++
+//     if (count >= 55) {
+//         break
+//     }
+// console.log(i)
+// }
+// console.log(`Count: ${count}`)
+
+
+// let i = 90;
+// while (true) {
+//     i=i-5
+//     console.log(i)
+//         if (i<=0){
+//             break
+//     }
+// }
+
+let i = 2;
+count = 0;
+while (i<=10000000) {
+    
+    document.write(`<h2>Loop ${i}</h2>`);
+    i=i+i;
+    count++;
+    if (count >= 20){
+        break
+    }
 }
+console.log(`Loop ${count}`)
